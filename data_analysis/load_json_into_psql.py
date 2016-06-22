3a0d9e58ada44fd49c5416391d5c193229474277
Dean Liu <windsurf_dean@yahoo.com>
2016-06-22 14:36:32 -0700

    Modified json->psql script to better handle directories, added small (100) test dataset.

--- data_analysis/load_json_into_psql.py
+++ data_analysis/load_json_into_psql.py
@@ -41,71 +41,63 @@ import json
 #---------- 
 logger = logging.getLogger()
  
-# data_dir = 'test_data'
-data_dir = '/Users/deanliu/git/GalvanizeDSI/capstone_project/data/Yelp/yelp_dataset_challenge_academic_dataset'
+data_dir = 'test_data'
+# data_dir = '/Users/deanliu/git/GalvanizeDSI/capstone_project/data/Yelp/yelp_dataset_challenge_academic_dataset'
+
 logger.info("Loading data from '{}'".format(data_dir))
 
-PG_CONN_STRING = "dbname='test_yelp' user='deanliu' host='localhost' port='5432'"
-dbconn = pg.connect(PG_CONN_STRING)
+# PG_CONN_STRING = "dbname='test_yelp' user='deanliu' host='localhost' port='5432'"
+# dbconn = pg.connect(PG_CONN_STRING)
+
+# db = 'yelp_data' # the ENTIRE Yelp academic dataset
+db = 'yelp_test' # the small testing ver of Yelp academic dataset
+
+dbconn = pg.connect(dbname=db, user='deanliu', host='localhost', port='5432')
 cursor = dbconn.cursor()
  
 file_counter = 0
 
 # pi = ProgressInfo(os.path.expanduser(data_dir))
 
-
 # --- created SQL table ---
-'''
-CREATE TABLE test_yelp_checkin (
-id INTEGER PRIMARY KEY,
-data JSONB NOT NULL
-);
-'''
 cursor.execute('''CREATE TABLE checkins (id INTEGER PRIMARY KEY, data JSONB NOT NULL);''')
 cursor.execute('''CREATE TABLE businesses (id INTEGER PRIMARY KEY, data JSONB NOT NULL);''')
 cursor.execute('''CREATE TABLE reviews (id INTEGER PRIMARY KEY, data JSONB NOT NULL);''')
 cursor.execute('''CREATE TABLE tips (id INTEGER PRIMARY KEY, data JSONB NOT NULL);''')
 cursor.execute('''CREATE TABLE users (id INTEGER PRIMARY KEY, data JSONB NOT NULL);''')
 
-for root, dirs, files in os.walk(os.path.expanduser(data_dir)):
-    # print files
 
-    for f in files:
-        fname = os.path.join(root, f)
+files = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
+files = [f for f in files if f.endswith('.json')] # look only at '.json' files
 
-        if not fname.endswith(".json"): # skip non-json files
-            continue
+for f in files:
+    fname = os.path.join(data_dir, f)
 
-        print 'Processing file: {}'.format(fname)
-        # pi.update(file_counter)
-        file_counter += 1
-
-        with open(fname) as js_file:
-
-            for line_count, js_line in enumerate(js_file):
-                # print line_count, js_line
-                js_type = json.loads(js_line)['type']
-
-                if js_type == 'checkin':
-                    cursor.execute("""INSERT INTO checkins(id, data) VALUES (%s, %s);""", (line_count, js_line))
-                elif js_type == 'business':
-                    cursor.execute("""INSERT INTO businesses(id, data) VALUES (%s, %s);""", (line_count, js_line))
-                elif js_type == 'review':
-                    cursor.execute("""INSERT INTO reviews(id, data) VALUES (%s, %s);""", (line_count, js_line))
-                elif js_type == 'tip':
-                    cursor.execute("""INSERT INTO tips(id, data) VALUES (%s, %s);""", (line_count, js_line))
-                elif js_type == 'user':
-                    cursor.execute("""INSERT INTO users(id, data) VALUES (%s, %s);""", (line_count, js_line))
-                else: # no js_typ match, FAULT!
-                    pass
-        dbconn.commit() # <--- this line saved my butt!
-
+    print 'Processing file: {}'.format(fname)
     # pi.update(file_counter)
+    file_counter += 1
 
-  
-logger.debug("Refreshing materialized views")
-# cursor.execute("""REFRESH MATERIALIZED VIEW sessions""");
-# cursor.execute("""ANALYZE""");
+    with open(fname) as js_file:
+
+        for line_count, js_line in enumerate(js_file):
+            # print line_count, js_line
+            js_type = json.loads(js_line)['type']
+
+            if js_type == 'checkin':
+                cursor.execute("""INSERT INTO checkins(id, data) VALUES (%s, %s);""", (line_count, js_line))
+            elif js_type == 'business':
+                cursor.execute("""INSERT INTO businesses(id, data) VALUES (%s, %s);""", (line_count, js_line))
+            elif js_type == 'review':
+                cursor.execute("""INSERT INTO reviews(id, data) VALUES (%s, %s);""", (line_count, js_line))
+            elif js_type == 'tip':
+                cursor.execute("""INSERT INTO tips(id, data) VALUES (%s, %s);""", (line_count, js_line))
+            elif js_type == 'user':
+                cursor.execute("""INSERT INTO users(id, data) VALUES (%s, %s);""", (line_count, js_line))
+            else: # no js_typ match, FAULT!
+                pass
+    dbconn.commit() # <--- this line saved my butt!
+
+# pi.update(file_counter)\
  
 dbconn.commit()
 dbconn.close()
