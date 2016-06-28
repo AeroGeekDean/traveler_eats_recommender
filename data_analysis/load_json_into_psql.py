b3b88100244e0b79ec84990a20531202a8602d6d
Dean Liu <windsurf_dean@yahoo.com>
2016-06-27 17:45:56 -0700

    Fixed progressbar for loading json into postgreSQL.

--- data_analysis/load_json_into_psql.py
+++ data_analysis/load_json_into_psql.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-# Found this script from http://blog.endpoint.com/2016/03/loading-json-files-into-postgresql-95.html
-#
+# Adopted from http://blog.endpoint.com/2016/03/loading-json-files-into-postgresql-95.html
  
 import os
 import sys
-import logging
+import json
  
 try:
     import psycopg2 as pg
@@ -13,38 +12,15 @@ except:
     print "Install psycopg2"
     exit(123)
  
-# try:
-#     import progressbar
-# except:
-#     print "Install progressbar2"
-#     exit(123)
- 
-import json
+try:
+    import progressbar
+except:
+    print "Install progressbar2"
+    exit(123)
+  
 
-#----------
-# class ProgressInfo:
- 
-#     def __init__(self, dir):
-#         files_no = 0
-#         for root, dirs, files in os.walk(dir):
-#             for file in files:
-#                 if file.endswith(".json"):
-#                     files_no += 1
-#         self.files_no = files_no
-#         print "Found {} files to process".format(self.files_no)
-#         self.bar = progressbar.ProgressBar(maxval=self.files_no,
-#                                            widgets=[' [', progressbar.Timer(), '] [', progressbar.ETA(), '] ', progressbar.Bar(),])
- 
-#     def update(self, file_counter):
-#         self.bar.update(file_counter)
-
-#---------- 
-logger = logging.getLogger()
- 
 data_dir = 'test_data'
 # data_dir = '/Users/deanliu/git/GalvanizeDSI/capstone_project/data/Yelp/yelp_dataset_challenge_academic_dataset'
-
-logger.info("Loading data from '{}'".format(data_dir))
 
 # PG_CONN_STRING = "dbname='test_yelp' user='deanliu' host='localhost' port='5432'"
 # dbconn = pg.connect(PG_CONN_STRING)
@@ -57,8 +33,6 @@ cursor = dbconn.cursor()
  
 file_counter = 0
 
-# pi = ProgressInfo(os.path.expanduser(data_dir))
-
 # --- created SQL table ---
 cursor.execute('''CREATE TABLE checkins (id INTEGER PRIMARY KEY, data JSONB NOT NULL);''')
 cursor.execute('''CREATE TABLE businesses (id INTEGER PRIMARY KEY, data JSONB NOT NULL);''')
@@ -68,19 +42,30 @@ cursor.execute('''CREATE TABLE users (id INTEGER PRIMARY KEY, data JSONB NOT NUL
 
 
 files = [f for f in os.listdir(data_dir) if os.path.isfile(os.path.join(data_dir, f))]
-files = [f for f in files if f.endswith('.json')] # look only at '.json' files
+json_files = [f for f in files if f.endswith('.json')]
 
-for f in files:
+print 'Found {} files to process'.format(len(json_files))
+
+for f in json_files:
     fname = os.path.join(data_dir, f)
 
     print 'Processing file: {}'.format(fname)
-    # pi.update(file_counter)
+    bar_max = os.path.getsize(fname)
+    bar = progressbar.ProgressBar(max_value = bar_max, 
+            widgets=[
+                ' [', progressbar.Timer(), '] ',
+                progressbar.Bar(),
+                ' (', progressbar.ETA(), ')',
+            ])
+    bar_progress = 0
     file_counter += 1
 
     with open(fname) as js_file:
 
         for line_count, js_line in enumerate(js_file):
             # print line_count, js_line
+            bar_progress+=len(js_line)
+            bar.update(bar_progress)
             js_type = json.loads(js_line)['type']
 
             if js_type == 'checkin':
@@ -95,11 +80,24 @@ for f in files:
                 cursor.execute("""INSERT INTO users(id, data) VALUES (%s, %s);""", (line_count, js_line))
             else: # no js_typ match, FAULT!
                 pass
+    bar.finish()
     dbconn.commit() # <--- this line saved my butt!
 
-# pi.update(file_counter)\
- 
 dbconn.commit()
 dbconn.close()
- 
-logger.info("Loaded {} files".format(file_counter))
+
+
+# # Define a main() function that prints a little greeting.
+# def main():
+#   # Get the name from the command line, using 'World' as a fallback.
+#   if len(sys.argv) >= 2:
+#     name = sys.argv[1]
+#   else:
+#     name = 'World'
+#   print 'Howdy', name
+#   print 'yay!'
+
+# # This is the standard boilerplate that calls the main() function.
+# if __name__ == '__main__':
+#   main()
+
